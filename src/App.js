1e7774154533d83114b9c5f17fd7a0d0fd90abc6
Keerthana Anandan <58841274+KeerthanaAnandan@users.noreply.github.com>
2021-07-16 17:28:52 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as  Router , Switch , Route} from "react-router-dom";
+import Nav from "./Components/Nav";
+import PersonalDetails from "./Components/PersonalDetails";
+import CompanyDetails from "./Components/CompanyDetails";
+import EmailVerification from "./Components/EmailVerification";
+import Email from "./Components/Email";
+import SuccessPage from "./Components/SuccessPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <Nav />
+      <Switch>
+        <Route path="/" exact>
+        
+        <PersonalDetails />
+        {/* <Email /> */}
+        </Route>
+        <Route path="/companydetails"  >
+       
+       <CompanyDetails />
+        </Route>
+        <Route path="/emailverification"  >
+       
+      <EmailVerification />
+        </Route>
+
+        <Route path="/success" exact>
+          <SuccessPage />
+        </Route>
+      </Switch>
+     
+      </Router>
+      
     </div>
+   
   );
 }
 
